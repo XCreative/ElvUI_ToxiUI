7129c5cd575e5e7796274c687dd3c70cfedf7645
XCreative <sonic.pub@gmail.com>
2022-02-23 19:52:20 +0000

    refactor(version): Versions bump
    Version bump for patch 9.2.0
    Set ElvUI Version to min 12.65

---
+++ Core/Changelog/5.3.7.lua
@@ -0,0 +1,15 @@
+local TXUI, F, E, I, V, P, G = unpack(select(2, ...))
+
+TXUI.Changelog["5.3.7"] = {
+    -- LuaFormatter off
+    HOTFIX = true,
+    CHANGES = {
+        "* General",
+        "Version bump for patch 9.2.0",	
+		"set ElvUI Version to min 12.65",	
+	
+        "* Hotfix",
+        "fixed Issue with Requirements for ToxiUI Vehicle Bar and SLE Vehiclebar is acctivated",
+    }
+    -- LuaFormatter on
+}
